1534b2743b74a45e0ffc7cbc354455bd2ebbbd7f
Stephane Decleire <stephane.decleire@cariboo-networks.com>
2012-11-22 18:19:13 +0100

    Transaction ID generated by the bundle : ids provided by SIPS are too limited

--- Plugin/SipsCheckoutPlugin.php
+++ Plugin/SipsCheckoutPlugin.php
@@ -3,6 +3,7 @@
 namespace Cariboo\Payment\SipsBundle\Plugin;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use JMS\Payment\CoreBundle\Model\ExtendedDataInterface;
 use JMS\Payment\CoreBundle\Model\FinancialTransactionInterface;
 use JMS\Payment\CoreBundle\Plugin\PluginInterface;
@@ -33,6 +34,8 @@ use Cariboo\Payment\SipsBundle\Client\Response;
 
 class SipsCheckoutPlugin extends AbstractPlugin
 {
+    const MAX_TRANSACTION_ID = 999999;       // Maximal transaction Id supported by SIPS
+
     protected $container;
 
     /**
@@ -79,67 +82,17 @@ class SipsCheckoutPlugin extends AbstractPlugin
     public function approveAndDeposit(FinancialTransactionInterface $transaction, $retry)
     {
         $request = $this->container->get('request');
-        $logger = $this->container->get('logger');
 
         if ($request->isMethod('POST')) {
             $data = $request->get('DATA');
 
+            // Process the transaction
             $response = $this->client->requestDoCheckoutPayment($data);
             $this->throwUnlessSuccessResponse($response, $transaction);
-
-            $logger->info('RESPONSE_CODE: '.$response->body->get('response_code'));
-            switch ($response->body->get('response_code')) {
-                case '00':      // Autorisation acceptée
-                    $transaction->setReferenceNumber($response->body->get('transaction_id'));
-                    $transaction->setProcessedAmount($this->client->convertAmountFromSipsFormat($response->body->get('amount'), $response->body->get('currency_code')));
-                    $transaction->setResponseCode(PluginInterface::RESPONSE_CODE_SUCCESS);
-                    $transaction->setReasonCode(PluginInterface::REASON_CODE_SUCCESS);
-                    break;
-                
-                case '17':      // Annulation de l'internaute
-                    $ex = new PaymentPendingException('PaymentAction cancelled.');
-                    $transaction->setReferenceNumber($response->body->get('transaction_id'));
-                    $transaction->setResponseCode('Cancelled');
-                    $transaction->setReasonCode($response->body->get('response_code').': '.$response->body->get('complementary_info'));
-                    $ex->setFinancialTransaction($transaction);
-                    throw $ex;
-                    break;
-                
-                case '02':      // Dépassement de plafond. Forçage possible par téléphone (selon contrat)
-                case '05':      // Autorisation refusée
-                    $ex = new FinancialException('PaymentAction failed.');
-                    $transaction->setReferenceNumber($response->body->get('transaction_id'));
-                    $transaction->setResponseCode('Failed');
-                    $transaction->setReasonCode($response->body->get('response_code').': '.$response->body->get('complementary_info'));
-                    $ex->setFinancialTransaction($transaction);
-                    throw $ex;
-                
-                case '34':      // Suspicion de fraude
-                case '75':      // Nombre de tentatives de saisie du numéro de carte dépassé
-                    $ex = new FinancialException('PaymentAction failed.');
-                    $transaction->setReferenceNumber($response->body->get('transaction_id'));
-                    $transaction->setResponseCode('Failed');
-                    $transaction->setReasonCode($response->body->get('response_code').': '.$response->body->get('complementary_info'));
-                    $ex->setFinancialTransaction($transaction);
-                    throw $ex;
-                
-                case '03':      // Champ merchant_id invalide ou contrat VAD inexistant
-                case '12':      // Transaction invalide : vérifier les paramètres de la requête
-                case '30':      // Erreur de format
-                case '90':      // Service temporairement indisponible
-                default:
-                    $ex = new InternalErrorException('PaymentAction failed.');
-                    $transaction->setReferenceNumber($response->body->get('transaction_id'));
-                    $transaction->setResponseCode('Failed');
-                    $transaction->setReasonCode($response->body->get('response_code').': '.$response->body->get('complementary_info'));
-                    $ex->setFinancialTransaction($transaction);
-                    throw $ex;
-            }
+            $this->process($transaction, $response);
         }
         else
         {
-            $logger->info('CHOOSE_CREDIT_CARD');
-
             $data = $transaction->getExtendedData();
             $token = $this->obtainSipsCheckoutToken($transaction);
 
@@ -150,86 +103,16 @@ class SipsCheckoutPlugin extends AbstractPlugin
 
             throw $actionRequest;
         }
+    }
 
-        // $details = $this->client->requestGetExpressCheckoutDetails($token);
-        // $this->throwUnlessSuccessResponse($details, $transaction);
+    public function processNotify(Request $request)
+    {
+        $data = $request->request->get('DATA');
 
-        // // verify checkout status
-        // switch ($details->body->get('CHECKOUTSTATUS')) {
-        //     case 'PaymentActionFailed':
-        //         $ex = new FinancialException('PaymentAction failed.');
-        //         $transaction->setResponseCode('Failed');
-        //         $transaction->setReasonCode('PaymentActionFailed');
-        //         $ex->setFinancialTransaction($transaction);
+        // Process the transaction
+        $response = $this->client->requestDoCheckoutPayment($data);
 
-        //         throw $ex;
-
-        //     case 'PaymentCompleted':
-        //         break;
-
-        //     case 'PaymentActionNotInitiated':
-        //         break;
-
-        //     default:
-        //         $actionRequest = new ActionRequiredException('User has not yet authorized the transaction.');
-        //         $actionRequest->setFinancialTransaction($transaction);
-        //         $actionRequest->setAction(new VisitUrl($this->client->getAuthenticateExpressCheckoutTokenUrl($token)));
-
-        //         throw $actionRequest;
-        // }
-
-        // // complete the transaction
-        // $data->set('sips_payer_id', $details->body->get('PAYERID'));
-
-        // $opts = $data->has('checkout_params') ? $data->get('checkout_params') : array();
-        // $opts['PAYMENTREQUEST_0_CURRENCYCODE'] = $transaction->getPayment()->getPaymentInstruction()->getCurrency();
-
-        // if ($data->has('checkout_items'))
-        // {
-        //     $itemsAmount = 0.00;
-        //     $idx = 0;
-        //     foreach($data->get('checkout_items') as $item)
-        //     {
-        //         $opts['L_PAYMENTREQUEST_0_ITEMCATEGORY' . $idx] = $item['category'];
-        //         $opts['L_PAYMENTREQUEST_0_NAME' . $idx] = $item['label'];
-        //         $opts['L_PAYMENTREQUEST_0_QTY' . $idx] = $item['quantity'];
-        //         $opts['L_PAYMENTREQUEST_0_AMT' . $idx] = $item['unit_price'];
-        //         $itemsAmount += $item['unit_price'] * $item['quantity'];
-        //         $idx++;
-        //     }
-        //     $opts['PAYMENTREQUEST_0_ITEMAMT'] = $itemsAmount;
-        // }
-        // $response = $this->client->requestDoExpressCheckoutPayment(
-        //     $data->get('sips_checkout_token'),
-        //     $transaction->getRequestedAmount(),
-        //     $paymentAction,
-        //     $details->body->get('PAYERID'),
-        //     $opts
-        // );
-        // $this->throwUnlessSuccessResponse($response, $transaction);
-
-        // switch($response->body->get('PAYMENTINFO_0_PAYMENTSTATUS')) {
-        //     case 'Completed':
-        //         break;
-
-        //     case 'Pending':
-        //         $transaction->setReferenceNumber($response->body->get('PAYMENTINFO_0_TRANSACTIONID'));
-                
-        //         throw new PaymentPendingException('Payment is still pending: '.$response->body->get('PAYMENTINFO_0_PENDINGREASON'));
-
-        //     default:
-        //         $ex = new FinancialException('PaymentStatus is not completed: '.$response->body->get('PAYMENTINFO_0_PAYMENTSTATUS'));
-        //         $ex->setFinancialTransaction($transaction);
-        //         $transaction->setResponseCode('Failed');
-        //         $transaction->setReasonCode($response->body->get('PAYMENTINFO_0_PAYMENTSTATUS'));
-
-        //         throw $ex;
-        // }
-
-        // $transaction->setReferenceNumber($response->body->get('PAYMENTINFO_0_TRANSACTIONID'));
-        // $transaction->setProcessedAmount($response->body->get('PAYMENTINFO_0_AMT'));
-        // $transaction->setResponseCode(PluginInterface::RESPONSE_CODE_SUCCESS);
-        // $transaction->setReasonCode(PluginInterface::REASON_CODE_SUCCESS);
+        return $response;
     }
 
     public function processes($paymentSystemName)
@@ -249,6 +132,14 @@ class SipsCheckoutPlugin extends AbstractPlugin
     protected function obtainSipsCheckoutToken(FinancialTransactionInterface $transaction)
     {
         $data = $transaction->getExtendedData();
+
+        if ($transaction->getTrackingId() == null) {
+            $em = $this->container->get('doctrine')->getEntityManager();
+            $transaction->setTrackingId($data->get('tracking_id'));
+            $em->persist($transaction);
+            $em->flush($transaction);
+        }
+
         if ($data->has('sips_checkout_token')) {
             return $data->get('sips_checkout_token');
         }
@@ -257,6 +148,13 @@ class SipsCheckoutPlugin extends AbstractPlugin
         $opts['normal_return_url'] = $this->getReturnUrl($data);
         $opts['cancel_return_url'] = $this->getCancelUrl($data);
         $opts['automatic_response_url'] = $this->getNotifyUrl($data);
+        $opts['order_id'] = $data->get('order_id');
+        $opts['data'] = $data->get('tracking_id');
+
+        // SIPS default transaction Id should not be used (based on time : can't manage more than 1 transaction per second !)
+        if ($data->has('transaction_id')) {
+            $opts['transaction_id'] = $data->get('transaction_id');
+        }
 
     //      $parm="$parm transaction_id=123456";
     //      $parm="$parm language=fr";
@@ -303,6 +201,62 @@ class SipsCheckoutPlugin extends AbstractPlugin
         // $transaction->setState(FinancialTransactionInterface::STATE_PENDING);
 
         return $encrypted;
+    }
+
+   /**
+     * @param \JMS\Payment\CoreBundle\Model\FinancialTransactionInterface $transaction
+     * @param \Cariboo\Payment\SipsBundle\Client\Response $response
+     * @return null
+     * @throws \JMS\Payment\CoreBundle\Plugin\Exception\FinancialException
+     */
+    protected function process($transaction, $response)
+    {
+        switch ($response->body->get('response_code')) {
+            case '00':      // Autorisation acceptée
+                $transaction->setReferenceNumber($response->body->get('transaction_id'));
+                $transaction->setProcessedAmount($this->client->convertAmountFromSipsFormat($response->body->get('amount'), $response->body->get('currency_code')));
+                $transaction->setResponseCode(PluginInterface::RESPONSE_CODE_SUCCESS);
+                $transaction->setReasonCode(PluginInterface::REASON_CODE_SUCCESS);
+                break;
+            
+            case '17':      // Annulation de l'internaute
+                $ex = new PaymentPendingException('PaymentAction cancelled.');
+                $transaction->setReferenceNumber($response->body->get('transaction_id'));
+                $transaction->setResponseCode('Cancelled');
+                $transaction->setReasonCode($response->body->get('response_code').': '.$response->body->get('complementary_info'));
+                $ex->setFinancialTransaction($transaction);
+                throw $ex;
+            
+            case '02':      // Dépassement de plafond. Forçage possible par téléphone (selon contrat)
+            case '05':      // Autorisation refusée
+                $ex = new FinancialException('PaymentAction failed.');
+                $transaction->setReferenceNumber($response->body->get('transaction_id'));
+                $transaction->setResponseCode('Failed');
+                $transaction->setReasonCode($response->body->get('response_code').': '.$response->body->get('complementary_info'));
+                $ex->setFinancialTransaction($transaction);
+                throw $ex;
+            
+            case '34':      // Suspicion de fraude
+            case '75':      // Nombre de tentatives de saisie du numéro de carte dépassé
+                $ex = new FinancialException('PaymentAction failed.');
+                $transaction->setReferenceNumber($response->body->get('transaction_id'));
+                $transaction->setResponseCode('Failed');
+                $transaction->setReasonCode($response->body->get('response_code').': '.$response->body->get('complementary_info'));
+                $ex->setFinancialTransaction($transaction);
+                throw $ex;
+            
+            case '03':      // Champ merchant_id invalide ou contrat VAD inexistant
+            case '12':      // Transaction invalide : vérifier les paramètres de la requête
+            case '30':      // Erreur de format
+            case '90':      // Service temporairement indisponible
+            default:
+                $ex = new InternalErrorException('PaymentAction failed.');
+                $transaction->setReferenceNumber($response->body->get('transaction_id'));
+                $transaction->setResponseCode('Failed');
+                $transaction->setReasonCode($response->body->get('response_code').': '.$response->body->get('complementary_info'));
+                $ex->setFinancialTransaction($transaction);
+                throw $ex;
+        }
     }
 
    /**
